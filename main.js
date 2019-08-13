534ac9fc20c4785a041523e79269043d7c5c2b54
cris.dux <crisfer.4217@gmail.com>
2019-08-13 16:13:11 -0400

    overlay finalizado

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ window.addEventListener('load', () =>{
     document.getElementById('grid').classList.add('imagenes-cargadas');
 
     const enlaces= document.querySelectorAll('#categorias a');
-
+    //filtrado de imagenes por cateogiras
     enlaces.forEach((elemento)=>{
         elemento.addEventListener('click',(e)=>{
             e.preventDefault();
@@ -21,4 +21,34 @@ window.addEventListener('load', () =>{
 
         });
     });
+
+    //filtrado por barra de busqueda 
+    document.querySelector('#barra-busqueda').addEventListener('input', (e) =>{
+        const busqueda = e.target.value;
+        grid.filter((item) => item.getElement().dataset.etiquetas.includes(busqueda));
+    });
+
+    //listener para las imagenes
+    const overlay = document.getElementById('overlay');
+    document.querySelectorAll('.grid .item img').forEach((elemento)=>{
+        
+        elemento.addEventListener('click',() =>{
+            const ruta = elemento.getAttribute('src');
+            const descripcion = elemento.parentNode.parentNode.dataset.descripcion;
+
+            overlay.classList.add('activo');
+            document.querySelector('#overlay img').src=ruta;
+            document.querySelector('#overlay .descripcion').innerHTML=descripcion;
+        })
+    });
+
+      //cerrar modal
+      document.querySelector('#btn-cerrar-modal').addEventListener('click',() =>{
+        overlay.classList.remove('activo');
+      });
+
+      //listerner del overlay
+      overlay.addEventListener('click',(e) =>{
+        e.target.id === 'overlay' ? overlay.classList.remove('activo') : '';
+      });
 });
