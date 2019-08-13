9c92cc757a645e84e9c29e9ce52b5834c70e64e0
cris.dux <crisfer.4217@gmail.com>
2019-08-13 12:49:50 -0400

    filtrado por categoria acabado

--- main.js
+++ main.js
@@ -7,4 +7,18 @@ const grid = new Muuri('.grid',{
 window.addEventListener('load', () =>{
     grid.refreshItems().layout();
     document.getElementById('grid').classList.add('imagenes-cargadas');
+
+    const enlaces= document.querySelectorAll('#categorias a');
+
+    enlaces.forEach((elemento)=>{
+        elemento.addEventListener('click',(e)=>{
+            e.preventDefault();
+            enlaces.forEach((enlace) => enlace.classList.remove('activo'));
+            e.target.classList.add('activo');
+
+            const categoria = e.target.innerHTML.toLowerCase();
+            categoria === 'todos' ? grid.filter('[data-categoria]') : grid.filter(`[data-categoria="${categoria}"]`);
+
+        });
+    });
 });
